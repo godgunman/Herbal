451d36c330efcdcb37be4996872af2b020f29083
godgunman <godgunman@gmail.com>
2013-05-16 04:21:26 +0800

    result button implemented

--- Herbal/src/tw/edu/ntust/dt/herbal/ResultActivity.java
+++ Herbal/src/tw/edu/ntust/dt/herbal/ResultActivity.java
@@ -5,12 +5,60 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ResultActivity extends Activity {
+
+	private ImageView buttonBackground;
+	private Button normalButton;
+	private Button mealButton;
+	private Button sportButton;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.result);
+		findViews();
+		setListeners();
+
+	}
+
+	private void findViews() {
+		buttonBackground = (ImageView) findViewById(R.id.result_button_background);
+		normalButton = (Button) findViewById(R.id.result_button_normal);
+		mealButton = (Button) findViewById(R.id.result_button_meal);
+		sportButton = (Button) findViewById(R.id.result_button_sport);
+
+	}
+
+	private void setListeners() {
+		normalButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				buttonBackground
+						.setImageResource(R.drawable.result_head_button_normal);
+			}
+		});
+
+		mealButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				buttonBackground
+						.setImageResource(R.drawable.result_head_button_meal);
+			}
+		});
+
+		sportButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				buttonBackground
+						.setImageResource(R.drawable.result_head_button_sport);
+			}
+		});
 	}
 
 	@Override
