4bef9c6a1b654b1ab7c387a94a65ec68455f3b85
crazyplum <crazyplum24@gmail.com>
2013-05-17 08:54:29 +0800

    confirmActivity not finished

--- Herbal/src/tw/edu/ntust/dt/herbal/ConfirmActivity.java
+++ Herbal/src/tw/edu/ntust/dt/herbal/ConfirmActivity.java
@@ -2,6 +2,8 @@ package tw.edu.ntust.dt.herbal;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class ConfirmActivity extends Activity {
 
@@ -11,7 +13,17 @@ public class ConfirmActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.confirm);
 		
-		
+		cart = getIntent().getExtras().getStringArray("cart");
+		generateCartList();
+	}
+	
+	private void generateCartList(){
+		LinearLayout parent = (LinearLayout) findViewById(R.id.herbal_carts);
+		parent.removeAllViews();
+		for(String item: cart){
+			ImageView image = new ImageView(this);
+			
+		}
 	}
 	
 }
