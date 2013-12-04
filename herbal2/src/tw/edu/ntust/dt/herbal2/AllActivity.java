9e921c26bb16353256b9f4d16ef67195078be960
godgunman <godgunman@gmail.com>
2013-12-04 15:09:33 +0800

    new herbal project init

---
+++ herbal2/src/tw/edu/ntust/dt/herbal2/AllActivity.java
@@ -0,0 +1,23 @@
+package tw.edu.ntust.dt.herbal2;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class AllActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cover);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
