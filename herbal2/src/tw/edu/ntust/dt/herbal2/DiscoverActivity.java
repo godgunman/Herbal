86f4272aabf70bda9a50b486f4029ab6b608e031
godgunman <godgunman@gmail.com>
2013-12-04 16:21:06 +0800

    build main UI-flow structure, adding static files

--- herbal2/src/tw/edu/ntust/dt/herbal2/DiscoverActivity.java
+++ herbal2/src/tw/edu/ntust/dt/herbal2/DiscoverActivity.java
@@ -1,23 +1,37 @@
 package tw.edu.ntust.dt.herbal2;
 
+import tw.edu.ntust.dt.herbal2.adapter.HerbalDiscoverAdapter;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
+import android.view.View;
 
 public class DiscoverActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cover);
-    }
+	private ViewPager pager;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_discover);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+		HerbalDiscoverAdapter adapter = new HerbalDiscoverAdapter(this);
+		pager = (ViewPager) findViewById(R.id.herbal_list);
+		pager.setAdapter(adapter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void completeDiscover(View view) {
+		Intent intent = new Intent();
+		intent.setClass(this, UploadActivity.class);
+		startActivity(intent);
+	}
 }
--- herbal2/src/tw/edu/ntust/dt/herbal2/ResultActivity.java
+++ herbal2/src/tw/edu/ntust/dt/herbal2/ResultActivity.java
@@ -1,23 +1,62 @@
 package tw.edu.ntust.dt.herbal2;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class ResultActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cover);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_result);
 
+		new AsyncTask<Void, Void, Void>() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+			@Override
+			protected Void doInBackground(Void... voids) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				return null;
+			}
+
+			protected void onPostExecute(Void result) {
+				findViewById(R.id.progressBar).setVisibility(View.GONE);
+				
+				Animation animationFadeIn = AnimationUtils.loadAnimation(
+						ResultActivity.this, R.anim.fadein);
+				
+				View afterLoading = findViewById(R.id.after_lading);
+				afterLoading.setVisibility(View.VISIBLE);
+				afterLoading.startAnimation(animationFadeIn);
+			};
+		}.execute();
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	/**
+	 * onClick for search_button
+	 * 
+	 * @param view
+	 */
+	public void clickSearchButton(View view) {
+		Intent intent = new Intent();
+		intent.setClass(this, DiscoverActivity.class);
+		startActivity(intent);
+	}
 }
