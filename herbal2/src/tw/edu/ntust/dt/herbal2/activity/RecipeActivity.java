17e089eb64fff27b5455c8de24aed8b96770828b
godgunman <godgunman@gmail.com>
2013-12-20 09:29:50 +0800

    add home button to back to coveractivity

--- herbal2/src/tw/edu/ntust/dt/herbal2/activity/RecipeActivity.java
+++ herbal2/src/tw/edu/ntust/dt/herbal2/activity/RecipeActivity.java
@@ -1,11 +1,15 @@
 package tw.edu.ntust.dt.herbal2.activity;
 
+import com.facebook.Session;
+
 import tw.edu.ntust.dt.herbal2.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.ImageView;
 import android.view.Menu;
+import android.view.View;
 
 public class RecipeActivity extends Activity {
 
@@ -24,6 +28,10 @@ public class RecipeActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void home(View view) {
+		startActivity(new Intent(this, CoverActivity.class));
 	}
 
 }
